e2aaf86603127acfa9cce573770e5f1c249a6d66
Omid Azizi <oazizi@pixielabs.ai>
2020-09-21 13:02:05 -0700

    Fix leaks in SizedType

--- src/types.h
+++ src/types.h
@@ -98,9 +98,9 @@ public:
 
 private:
   bool is_signed_ = false;
-  SizedType *element_type_ = nullptr; // for "container" and pointer
-                                      // (like) types
-  size_t num_elements_;               // for array like types
+  std::shared_ptr<SizedType> element_type_; // for "container" and pointer
+                                            // (like) types
+  size_t num_elements_; // for array like types
   std::string name_; // name of this type, for named types like struct
   bool ctx_ = false; // Is bpf program context
   AddrSpace as_ = AddrSpace::none;
@@ -166,13 +166,13 @@ public:
   const SizedType *GetElementTy() const
   {
     assert(IsArrayTy());
-    return element_type_;
+    return element_type_.get();
   }
 
   const SizedType *GetPointeeTy() const
   {
     assert(IsPtrTy());
-    return element_type_;
+    return element_type_.get();
   }
 
   bool IsBoolTy() const
